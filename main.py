aeddc6b9304f8e0feaaf4381d9badde362cd536f
Joao Pedro Tambosi <joaodupe2@gmail.com>
2023-09-18 08:46:50 -0300

    Adicionei a parte de DocTest e DocString feita pelo Vilas por problemas no GitHub dele

--- main.py
+++ main.py
@@ -8,18 +8,35 @@ Created on Mon Sep 18 07:47:06 2023
 from funcao import calcula_dif
 from retorna_arquivo import retorna_arquivo
 
-# Tenta ler as duas opções
-datas = input("Forneça o nome do arquivo ou a data desejada:")
-
-if "-" in datas: 
-    datas = datas
+def main():
+    """
+    Função que calcula a diferença de dias entre duas datas.
     
-else: 
-    datas = retorna_arquivo(datas)
+    Essa função recebe duas datas em um input ou lê a partir de um arquivo, calcula os dias entre as duas datas, e printa o resultado.
+    
+    Example:
+    >>> # Simula o input de duas datas
+    >>> # Forneça o nome do arquivo ou a data desejada: 22 de Agosto de 2023 - 23 de Agosto de 2023
+    >>> # Número de dias entre as duas datas: 1
+    >>> main()
 
+    >>> # Simula o input de um arquivo
+    >>> # Forneça o nome do arquivo ou a data desejada: texto_datas
+    >>> # Número de dias entre as duas datas: 287
+    >>> main()
 
-dif_datas = calcula_dif(datas)
+    """
+    datas = input("Forneça o nome do arquivo ou a data desejada:")
 
-# Print days:
-print("Número de dias entre as duas datas:", dif_datas)
+    if "-" in datas: 
+        datas = datas
+    else: 
+        datas = retorna_arquivo(datas)
 
+    dif_datas = calcula_dif(datas)
+
+    # Print days:
+    print("Número de dias entre as duas datas:", dif_datas)
+
+if __name__ == "__main__":
+    main()
